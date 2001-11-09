31e2e452a5fa9ee2d878cc68c912cfc321b430e1
Denis Kokarev <d_kokarev@mail.ru>
2001-11-09 04:09:51 +0000

    Pre-alpha of UI

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 #include "game.h"
 #include "minimax.h"
 
-void print_position(const GAME_STATE *state) {
+void print_position(const GAME_STATE state) {
 	int		x, y;
 	char	ch;
 
 	for (y = 0; y < MAX_DIM; y++) {
 		for (x = 0; x < MAX_DIM; x++) {
-			switch ((*state)[x][y]) {
+			switch (state[x][y]) {
 			case COLOR_WHITE:
 				ch = '+';
 				break;
@@ -27,7 +27,7 @@ void print_position(const GAME_STATE *state) {
 	}
 }
 
-void read_position(GAME_STATE *state, FILE *fin) {
+void read_position(GAME_STATE state, FILE *fin) {
 	int		x = 0, y = 0;
 	char	ch;
 
@@ -35,13 +35,13 @@ void read_position(GAME_STATE *state, FILE *fin) {
 		ch = fgetc(fin);
 		switch(ch) {
 		case '+':
-			(*state)[x++][y] = COLOR_WHITE;
+			state[x++][y] = COLOR_WHITE;
 			break;
 		case '*':
-			(*state)[x++][y] = COLOR_BLACK;
+			state[x++][y] = COLOR_BLACK;
 			break;
 		case '.':
-			(*state)[x++][y] = COLOR_VACANT;
+			state[x++][y] = COLOR_VACANT;
 			break;
 		case '\n':
 			y++;
@@ -54,70 +54,127 @@ void read_position(GAME_STATE *state, FILE *fin) {
 	}
 }
 
+void usage(char *cmd_name) {
+	printf("Game \"Reversy\"\n\
+Usage: %s [-r <file> [-t <first turn color>]] [-c <your color>]\n\
+\t[-r <file>] read startup game position\n\
+\t[-t <first turn color>] who's moving first\n\
+\t[-c <your color>] your chips color\n\
+<color> can be '+' or '*'\n\
+<file> wiht startup position should have 8 rows with 8 space delimetered fields\n\
+'+' or '*' chips, or '.' for vacant place\n\
+This is interactive program the input command list is:\n\
+<x>:<y> - make turn at x:y position\n\
+write:<file name> - write current position to file\n\
+exit - exit the game\n", cmd_name); 
+}
+
 int main(int argc, char *argv[]) {
 	char		*fname = "";
+	char		command[32];
 	signed char		c;
 	FILE		*fin;
 	GAME_STATE	state;
+	GAME_TURN	turn;
+	GAME_TURN	turn_list[MAX_DIM * MAX_DIM];
 	int			e_code = 0;
+	int			i;
 
-	char		m_scolor[16];
-	GAME_TURN	turn = {0, 0, COLOR_VACANT};
-	GAME_TURN	next_turn = {0, 0, COLOR_VACANT};
-	GAME_TURN_HIST	turn_hist;
+	char		scolor[2];
+	CHIP_COLOR	c_who_is_first = COLOR_BLACK;
+	CHIP_COLOR	c_computer = COLOR_WHITE;
+	CHIP_COLOR	c_user = COLOR_BLACK;
 
 	opterr = 0;
-	while((c = getopt(argc, argv, "f:m:l:")) > 0) {
+	while((c = getopt(argc, argv, "r:t:c:")) > 0) {
 		switch(c) {
-		case 'f':
+		case 'r':
 			fname = optarg;
 			break;
-		case 'm':
-			if (sscanf(optarg, "%[+*]:%d:%d", m_scolor, &turn.x, &turn.y) != 3) {
-				printf("Error in m: option, it should be [+|*]:x:y\n");
+		case 't':
+			if (sscanf(optarg, "%[+*]", scolor) != 1) {
+				printf("Error in t: option, it should be -t <+|*>\n");
 				exit(1);
 			}
-			turn.color = ((m_scolor[0] == '+') ? COLOR_WHITE : COLOR_BLACK);
-			turn.x--;
-			turn.y--;
+			c_who_is_first = ((scolor[0] == '+') ? COLOR_WHITE : COLOR_BLACK);
 			break;
-		case 'l':
-			if (sscanf(optarg, "%[+*]", m_scolor) != 1) {
-				printf("Error in l: option, it should be [+|*]\n");
+		case 'c':
+			if (sscanf(optarg, "%[+*]", scolor) != 1) {
+				printf("Error in l: option, it should be -c <+|*>\n");
 				exit(1);
 			}
-			next_turn.color = ((m_scolor[0] == '+') ? COLOR_WHITE : COLOR_BLACK);
+			c_user = ((scolor[0] == '+') ? COLOR_WHITE : COLOR_BLACK);
+			c_computer = ((scolor[0] == '+') ? COLOR_BLACK : COLOR_WHITE);
 			break;
 		case '?':
 			printf("Unknown option %c\n", c);
+		case 'h':
+			usage(argv[0]);
+		default:
 			exit(1);
+		}
+	}
+	if (fname[0]) {
+		if ((fin = fopen(fname, "r"))) {
+			printf("Reading from %s ...\n", fname);
+			read_position(state, fin);
+		} else {
+			perror("Can't open game position file");
+			exit(1);
+		}
+	} else {
+		memset(state, COLOR_VACANT, sizeof(GAME_STATE));
+		state[3][3] = COLOR_BLACK;
+		state[3][4] = COLOR_WHITE;
+		state[4][4] = COLOR_BLACK;
+		state[4][3] = COLOR_WHITE;
+	}
+	printf("Starting position is:\n");
+	print_position(state);
+	turn.color = c_who_is_first;
+	do {
+		if (game_is_over(state)) {
+			printf("Game is over\n");
+			exit(0);
+		}
+		printf("your command>");
+		fflush(stdin);
+		scanf("%32s\n", command);
+		switch(command[0]) {
+		case 'e':
+			/* exit */
+			printf("Bye\n");
+			exit(0);
+		case 'p':
+			/* possible turns */
+			printf("Possible turns by color %d\n", turn.color);
+			for (i=make_turn_list(turn_list, state, turn.color)-1; i>=0; i--) {
+				printf("[%d:%d]\n", turn_list[i].x+1, turn_list[i].y+1);
+			}
 			break;
 		default:
+			if (sscanf(command, "%d:%d", &turn.x, &turn.y) != 2) {
+				printf("Thinking...\n");
+				find_best_turn(&turn, state, turn.color, 4);
+				if (turn.x < 0) {
+					printf("There are no turns by color %d\n", turn.color);
+					turn.color = (turn.color == COLOR_WHITE) ? COLOR_BLACK : COLOR_WHITE;
+					continue;
+				}
+			} else {
+				turn.x --;
+				turn.y --;
+			}
+			printf("Making turn by '%d' to [%d:%d]\n", turn.color, turn.x+1, turn.y+1);
+			if (! (e_code = validate_turn(state, &turn))) {
+				printf("We captured %d chip(s)\n", make_turn(state, &turn));
+				print_position(state);
+				turn.color = (turn.color == COLOR_WHITE) ? COLOR_BLACK : COLOR_WHITE;
+			} else {
+				printf("Invalid turn. Error %d\n", e_code);
+			}
+			fflush(stdout);
 		}
-	}
-	if (! fname[0]) {
-		printf("File name must be specified\n");
-		exit(1);
-	}
-	if ((fin = fopen(fname, "r"))) {
-		read_position(&state, fin);
-		print_position(&state);
-	}
-	if (turn.color != COLOR_VACANT) {
-		printf("Making turn by '%s' to [%d:%d]\n", m_scolor, turn.x+1, turn.y+1);
-		if ((e_code = validate_turn(&state, &turn))) {
-			printf("Invalid turn. Error %d\n", e_code);
-			exit(1);
-		}
-		printf("We captured %d chip(s)\n", game_turn(&state, &turn));
-		print_position(&state);
-	}
-	if (next_turn.color != COLOR_VACANT) {
-		log_turn(&turn_hist, &next_turn);
-		while(get_next_turn(&next_turn, &state, &turn_hist)) {
-			printf("[%d:%d]\n", next_turn.x+1, next_turn.y+1);
-			log_turn(&turn_hist, &next_turn);
-		}
-	}
+	} while(!feof(stdin));
 	return 0;
 }
