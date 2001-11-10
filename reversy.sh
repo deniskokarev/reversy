ffd57aae2ce0258bb6630ab3236501b37b498e4e
Denis Kokarev <d_kokarev@mail.ru>
2001-11-10 20:47:13 +0000

    Initial revision

---
+++ reversy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+reversy=./reversy
+tmp_in=/tmp/rev$$.in
+tmp_out=/tmp/rev$$.out
+log=reversy$$.log
+color='+'
+i=1
+trap "rm -f $tmp_in $tmp_out 2>/dev/null" INT TERM HUP EXIT
+echo "[$i] Making turn by color $color" 
+$reversy -l $log -c "$color" >$tmp_out
+while true; do
+	((i=i+1))
+	if [ "$color" = "+" ]; then
+		color="*"
+	else
+		color="+"
+	fi
+	mv $tmp_out $tmp_in
+	echo "[$i] Making turn by color $color" 
+	$reversy -i $tmp_in -l $log -c "$color" >$tmp_out
+	e_code=$?
+	if [ "$e_code" -eq "255" ]; then
+		break
+	else
+		if [ "$e_code" -eq "254" ]; then
+			echo "[$i] Color $color is skipping a turn"
+		else
+			if [ "$e_code" -ne "0" ]; then
+				echo -e "Error occurred.\n$reversy exit code is $e_code"
+				exit $e_code
+ 			fi
+		fi
+	fi
+done
+echo -e "Game is over\nFinal position:"
+cat $tmp_out
