930008bcae25f49882b3ca13abdaa469a3674aa0
Denis Kokarev <d_kokarev@mail.ru>
2001-12-02 00:22:00 +0000

    GAME_SCORE_MAX and GAME_SCORE_MIN bug fixed

--- minimax.h
+++ minimax.h
@@ -2,18 +2,27 @@
 #define __MINIMAX_H__
 
 #include "game.h"
+#include <limits.h>
 
-#define CUT_NO				0
-#define CUT_MORE_THAN		1
-#define CUT_LESS_THAN		-1
+#define	GAME_SCORE			int
+#define GAME_SCORE_MAX		INT_MAX-1
+#define GAME_SCORE_MIN		INT_MIN+1
+
+/* Use Alpha-Beta restrictions (several times faster) */
+#define ALPHA_BETA_CUT_ON
 
 /*
  * Find best turn on position 'state' by color 'color'
  * Look ahead in 'depth' moves
+ * Stop search if estimation simt (stop if more than) reached
+ * The last argument lets to cut branches which will not
+ * make sense for the final score evaluation, the obviously loosing
+ * ones. In game theory this approach is called Alpha-Beta restrictions. 
  */
-double find_best_turn(GAME_TURN *best_turn, 
+GAME_SCORE find_best_turn(GAME_TURN *best_turn, 
 					  const GAME_STATE state,
 					  CHIP_COLOR color,
-					  int depth);
+					  int depth,
+					  GAME_SCORE simt);
 
 #endif /* #ifdnef __MINIMAX_H__ */
