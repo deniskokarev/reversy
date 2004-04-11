72e00cc001cf0e6171b3b582172113aab712e3a8
Denis Kokarev <d_kokarev@mail.ru>
2004-04-11 21:22:56 +0000

    Test script for `reversy` binary. *.log files must match for each run This way you'll detect the difference in behavior if you broke something.

--- reversy.sh
+++ reversy.sh
@@ -5,10 +5,11 @@ tmp_in=/tmp/rev$$.in
 tmp_out=/tmp/rev$$.out
 log=reversy$$.log
 color='+'
+srch_depth=5
 i=1
-trap "rm -f $tmp_in $tmp_out 2>/dev/null" INT TERM HUP EXIT
+#trap "rm -f $tmp_in $tmp_out 2>/dev/null" INT TERM HUP EXIT
 echo "[$i] Making turn by color $color" 
-$reversy -l $log -c "$color" >$tmp_out
+$reversy -d $srch_depth -l $log -c "$color" >$tmp_out
 while true; do
 	((i=i+1))
 	if [ "$color" = "+" ]; then
@@ -18,7 +19,7 @@ while true; do
 	fi
 	mv $tmp_out $tmp_in
 	echo "[$i] Making turn by color $color" 
-	$reversy -i $tmp_in -l $log -c "$color" >$tmp_out
+	$reversy -i $tmp_in -d $srch_depth -l $log -c "$color" >$tmp_out
 	e_code=$?
 	if [ "$e_code" -eq "255" ]; then
 		break
@@ -35,3 +36,5 @@ while true; do
 done
 echo -e "Game is over\nFinal position:"
 cat $tmp_out
+cat $tmp_out >>$log
+rm -f $tmp_in $tmp_out 2>/dev/null
