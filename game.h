1e73808bb6296cb973b3afd0ab9e1053b2eea040
Denis Kokarev <d_kokarev@mail.ru>
2017-04-09 18:43:54 -0400

    making comments doxygen compliant

--- game.h
+++ game.h
@@ -1,23 +1,36 @@
+/**
+ * @file
+ * @brief Reversy Game backend for player vs player mode
+ *
+ * Contains all functions for basic game logic, such as validating and making turns
+ * @author Denis Kokarev
+ */
 #ifndef __GAME_H__
 #define __GAME_H__
 
+/** board size */
 #define MAX_DIM			8
 
+/**
+ * @brief it is very convenient to encode opposite sides colors as opposite numbers
+ */
 typedef signed char CHIP_COLOR;
 
-#define	COLOR_VACANT	((CHIP_COLOR)0)
-#define	COLOR_POS		((CHIP_COLOR)1)
-#define	COLOR_NEG		((CHIP_COLOR)-1)
+#define	COLOR_VACANT	((CHIP_COLOR)0)		/**< unoccupied space */
+#define	COLOR_POS		((CHIP_COLOR)1)		/**< WHITE is convenient to encode as positive */
+#define	COLOR_NEG		((CHIP_COLOR)-1)	/**< BLACK is convenient to encode as negative */
 
-/*
- * This is a Reversy game field 8x8 grid
+/**
+ * @brief This is a Reversy game field 8x8 grid
+ *
+ * First dimension is X (column) second dimension is Y (row)
  */
 typedef struct {
 	CHIP_COLOR b[MAX_DIM][MAX_DIM];
 } GAME_STATE;
 	 
-/*
- * Every turn has X and Y coordinates
+/**
+ * @brief Every turn has X (column) and Y (row) coordinates and the color
  */
 typedef struct tagGAME_TURN {
 	CHIP_COLOR		color;
@@ -33,34 +46,52 @@ typedef struct tagGAME_TURN {
 #define E_NO_FLIPS	3
 #define E_NO_TURNS	4
 
-/*
+/**
+ * @brief get the list of all possible turns at the given position for the side color
+ *
  * Turns generator. Don't use it in automatic procedures, because it is slow.
  * Makes a list of possible turns in game position 'state' by color 'color'
  * returns 0 if no possible turns left
+ * @param[out] turn - 1D buffer capable of holding all possible turns at the position state with color color @see GAME_TURN
+ * @param[in] state - current game position
+ * @param[in] color - which side do we want to make turns for
+ * @return numer of possible turns populated in turn buffer, where 0 means there is no possible turn for this color
  */
 int make_turn_list(GAME_TURN turn[MAX_DIM * MAX_DIM], const GAME_STATE *state, CHIP_COLOR color);
 
-/* 
- * Make turn on position 'state'.
+/**
+ * @brief Make a turn at the position 'state'
+ *
+ * Try to make a turn at the given board position. If the turn is legit perform all relevant chip captures
+ * @param[in,out] state - game board gets updated if the turn is indeed legitimate
+ * @param[in] turn - the coordinates and the color of the chip to be placed on the board
+ * @return E_OK if the turn was valid, otherwise return E_* error
  */
 int make_turn(GAME_STATE *state, const GAME_TURN *turn);
 
-/*
- * Validate turn
- * returns
+/**
+ * @brief Validate turn without actually making it
+ * @param[in] state - current board position
+ * @param[in] turn - try to make this turn
+ * @returns
  * E_OK - Ok
  * E_OCC - is already occupied
  * E_NO_FLIPS - no flips
  */
 int validate_turn(const GAME_STATE *state, const GAME_TURN *turn);
 
-/*
- * Count chips of certain color on position 'state'
+/**
+ * @brief Count chips of certain color on position 'state'
+ * @param[in] state - current game board state
+ * @param[in] color - count chips of the given color
  */
 int chips_count(const GAME_STATE *state, CHIP_COLOR color);
 
-/*
- * Game is over when neither color can make a turn
+/**
+ * @brief Check if the game is over.
+ *
+ * Game is over when neither side can make a turn
+ * @param[in] state - current game position
  */
 int game_is_over(const GAME_STATE *state);
 
--- minimax.h
+++ minimax.h
@@ -1,28 +1,57 @@
+/**
+ * @file
+ * @brief Reversy Game backend for player vs computer mode
+ *
+ * Has classic miniax implementatoin with alpha/beta pruning.
+ * @author Denis Kokarev
+ */
 #ifndef __MINIMAX_H__
 #define __MINIMAX_H__
 
 #include "game.h"
+#include <limits.h>
 
-#define	GAME_SCORE			short int
-#define GAME_SCORE_MAX		0x7FFF-1
-#define GAME_SCORE_MIN		0x8000+1
+/** our score will fit into 16bit val */
+#define GAME_SCORE			short
+#define GAME_SCORE_MAX		SHRT_MAX ///< 0x7FFF-1
+#define GAME_SCORE_MIN		SHRT_MIN ///< 0x8000+1
 
-/*
- * Find best turn on position 'state' by color 'color'
+/**
+ * @brief run minimax search for the best turn
+ *
+ * Find best turn on position 'state' by side 'color'
  * Look ahead in 'depth' moves
+ * It just invokes find_best_turn_intr()
+ * @see find_best_turn_intr()
+ * @param[out] best_turn - the best turn at the given position state
+ * @param[in] state - given position
+ * @param[in] color - side that will be making a turn
+ * @param[in] depth - how many steps ahead should we traverse the search space
+ * @return - the best potentially achievable score if both sides to play optimally
  */
 GAME_SCORE find_best_turn(GAME_TURN *best_turn, 
 					  const GAME_STATE *state,
 					  CHIP_COLOR color,
 					  int depth);
 
-/*
- * Similar to find_best_turn(), but with slightly more control:
- * Stop search if estimation simt (stop if more than) reached
- * The argument cuts branches which will not make sense for the final score evaluation -
- * the obviously loosing ones. In game theory this approach is called Alpha-Beta restrictions.
- * simt should be GAME_SCORE_MAX in manual invocation.
- * is_stop() is for user abort verification
+/**
+ * @brief run flexible minimax search for the best turn
+ *
+ * Find best turn on position 'state' by side 'color'
+ * Look ahead in 'depth' moves
+ * Similar to find_best_turn(), but more flixible.
+ * It observes the alpha/beta pruning by comparing the score with given simt score.
+ * It can also perform some periodic auxiliary action during the search or abort the
+ * search
+ * @param[out] best_turn - the best turn at the given position state
+ * @param[in] state - given position
+ * @param[in] color - side that will be making a turn
+ * @param[in] depth - how many steps ahead should we traverse the search space
+ * @param[in] simt - max score to consider or just finish the search performing alpha/beta pruning. Start with GAME_SCORE_MAX
+ * @param[in] is_stop - custom user function to be called at each search node.
+ *	this way you can update the search status on the screen or handle the user abort
+ * @param[in,out] is_stop_param - an argument to be passed to the custom user function
+ * @return - the best potentially achievable score if both sides to play optimally
  */
 GAME_SCORE find_best_turn_intr(GAME_TURN *best_turn,
 							   const GAME_STATE *state,
